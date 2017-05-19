8c7cdd509ad116a2d8d31d2ea3b2fcc70b97c0fd
Yasmine <yminachi@pillartechnology.com>
2017-05-19 16:26:36 -0400

    gradle can now publish to s3

---
+++ src/main/java/com/yasi/commonSpike/commonSpike/SomeCommonClass.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright © 2001-2017 HealthEdge Software, Inc. All Rights Reserved.
+ * This software is proprietary information of HealthEdge Software, Inc. and may not be reproduced or redistributed for any purpose.
+ */
+package com.yasi.commonSpike.commonSpike;
+
+public class SomeCommonClass {
+    public String someCommonClass() {
+        return "Hi";
+    }
+}
