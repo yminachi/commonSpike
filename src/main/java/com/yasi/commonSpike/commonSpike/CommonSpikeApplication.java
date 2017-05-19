66db886120f87dc5cbe486a8d8bf2b25b780380f
Yasmine <yminachi@pillartechnology.com>
2017-05-19 13:34:43 -0400

    initial commit

---
+++ src/main/java/com/yasi/commonSpike/commonSpike/CommonSpikeApplication.java
@@ -0,0 +1,12 @@
+package com.yasi.commonSpike.commonSpike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonSpikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonSpikeApplication.class, args);
+	}
+}
